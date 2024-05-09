86b3667c4972ec73e9c1343c345517cab84ebb86
Udi Oron <udioron@gmail.com>
2024-05-09 12:51:32 +0300

    added admin for expense

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from expenses.models import Expense
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "amount",
+        "date",
+    ]
+
+    search_fields = [
+        "id",
+        "title",
+        "amount",
+        "date",
+    ]
+
+    date_hierarchy = "date"
