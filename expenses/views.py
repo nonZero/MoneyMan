72a676bb6301a39fc1dd517176e9933b21c0fec5
Udi Oron <udioron@gmail.com>
2024-05-09 16:10:35 +0300

    template examples

--- expenses/views.py
+++ expenses/views.py
@@ -8,18 +8,11 @@ from .models import Expense
 
 def expense_list(request: HttpRequest):
     # qs = Expense.objects.all()
-    n = random.randint(1, 100)
-    colors = ["purple", "green", "blue"]
-    foo = 12345
-    person = {"name": "aviv", "phone": "1234567"}
     return render(
         request,
         "expenses/expense_list.html",
         {
-            "colors": colors,
-            "mashu": foo,
-            "kuku": "abcdefg",
-            "p": person,
-            "n": n,
+            "nums": [(i, i**2) for i in range(10)],
+            "grays": range(0, 256, 16),
         },
     )
