063c9c7563a96d438f81a5fe1f12901555826857
Udi Oron <udioron@gmail.com>
2024-05-09 14:17:07 +0300

    added Expense.__str__

--- expenses/models.py
+++ expenses/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 # ORM: Object Relational Mapper
 
+
 class Expense(models.Model):
     title = models.CharField(max_length=500)
     amount = models.DecimalField(decimal_places=2, max_digits=12)
     date = models.DateField()
+
+    def __str__(self):
+        return f"[#{self.id}] {self.amount} @ {self.date} {self.title}"
