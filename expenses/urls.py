f7df724a2d5eecee3a84259ef3052ed53a4157d9
Udi Oron <udioron@gmail.com>
2024-05-22 09:30:22 +0300

    added url names

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "expenses"
+
 urlpatterns = [
-    path("", views.expense_list),
-    path("<int:id>/", views.expense_detail),
+    path("", views.expense_list, name="list"),
+    path("expense/<int:id>/", views.expense_detail, name="detail"),
 ]
