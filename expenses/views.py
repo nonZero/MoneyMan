02c7e7ec88181b8331ab35d614761237d4042aa6
Udi Oron <udioron@gmail.com>
2024-05-22 10:45:33 +0300

    add recent only paramter on search

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 
@@ -10,12 +12,17 @@ def expense_list(request: HttpRequest):
     if q := request.GET.get('q', ''):
         qs = qs.filter(title__icontains=q)
 
+    if recent_only := request.GET.get('recent_only', '') == "on":
+        d = datetime.date.today() - datetime.timedelta(days=90)
+        qs = qs.filter(date__gte=d)
+
     return render(
         request,
         "expenses/expense_list.html",
         {
             "object_list": qs[:20],
             "q": q,
+            "recent_only": recent_only,
         },
     )
 
