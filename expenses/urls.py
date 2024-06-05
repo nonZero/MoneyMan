7bac4552624e243ea82539648c38317f3a8b56da
Udi Oron <udioron@gmail.com>
2024-06-05 14:43:10 +0300

    feat: move to class-based views

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,6 +7,8 @@ app_name = "expenses"
 
 urlpatterns = [
     path("", ExpenseListView.as_view(), name="list"),
-    path("add/", views.expense_create, name="create"),
+    path("add/", views.ExpenseCreateView.as_view(), name="create"),
     path("expense/<int:pk>/", views.ExpenseDetailView.as_view(), name="detail"),
+    path("expense/<int:pk>/edit/", views.ExpenseUpdateView.as_view(), name="update"),
+    path("expense/<int:pk>/delete/", views.ExpenseDeleteView.as_view(), name="delete"),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,10 +1,9 @@
 import datetime
 
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
 from . import forms
 from .models import Expense
@@ -12,6 +11,7 @@ from .models import Expense
 
 class ExpenseMixin(LoginRequiredMixin):
     model = Expense
+
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
 
@@ -47,26 +47,43 @@ class ExpenseDetailView(ExpenseMixin, DetailView):
     pass
 
 
-@login_required
-def expense_create(request: HttpRequest):
-    if request.method == "POST":
-        form = forms.ExpenseForm(request.POST)
-        if form.is_valid():
-            # d = form.cleaned_data
-            # o = Expense(**d)
-            # o.save()
-            form.instance.user = request.user
-            o = form.save()
-            # TODO: show success message
-            return redirect(o)
-            # return redirect("expenses:list")
-    else:
-        form = forms.ExpenseForm()
+class ExpenseCreateView(ExpenseMixin, SuccessMessageMixin, CreateView):
+    form_class = forms.ExpenseForm
+    success_message = "Expense '%(title)s' created."
 
-    return render(
-        request,
-        "expenses/expense_form.html",
-        {
-            "form": form,
-        },
-    )
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ExpenseUpdateView(ExpenseMixin, SuccessMessageMixin, UpdateView):
+    form_class = forms.ExpenseForm
+    success_message = "Expense '%(title)s' updated."
+
+
+class ExpenseDeleteView(ExpenseMixin, SuccessMessageMixin, DeleteView):
+    success_url = reverse_lazy("expenses:list")
+
+    def get_success_message(self, cleaned_data):
+        return f"Expense #{self.kwargs['pk']} deleted."
+
+
+# @login_required
+# def expense_create(request: HttpRequest):
+#     if request.method == "POST":
+#         form = forms.ExpenseForm(request.POST)
+#         if form.is_valid():
+#             form.instance.user = request.user
+#             o = form.save()
+#             messages.success(request, f"Expense #{o.id} created.")
+#             return redirect(o)
+#     else:
+#         form = forms.ExpenseForm()
+#
+#     return render(
+#         request,
+#         "expenses/expense_form.html",
+#         {
+#             "form": form,
+#         },
+#     )
